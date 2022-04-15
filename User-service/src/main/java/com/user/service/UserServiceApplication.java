9c38ba3afa4ef356882e50f38e471d1543b3ff81
alpetrovich <alexispetrovich11@gmail.com>
2022-04-15 12:01:25 -0300

    eureka service

--- User-service/src/main/java/com/user/service/UserServiceApplication.java
+++ User-service/src/main/java/com/user/service/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //notacion para habilitar al cliente FEING, para que pueda consumir los servicios
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- motorcycle-service/src/main/java/com/motorcicle/service/MotorcycleServiceApplication.java
+++ motorcycle-service/src/main/java/com/motorcicle/service/MotorcycleServiceApplication.java
@@ -2,12 +2,14 @@ package com.motorcicle.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class MotorcicleServiceApplication {
+@EnableEurekaClient
+public class MotorcycleServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MotorcicleServiceApplication.class, args);
+		SpringApplication.run(MotorcycleServiceApplication.class, args);
 	}
 
 }
